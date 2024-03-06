f554094e772de36133a3f7fe312588f3b0e90f03
228780975 <49092465+228780975@users.noreply.github.com>
2024-03-06 18:47:18 +0800

    Update template.sh

--- template.sh
+++ template.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 
+## Define your Cloudflare API key and email
+CLOUDFLARE_API_KEY=your_api_key
+CLOUDFLARE_EMAIL=your_email
+
+## Define the domain and record you want to update
+RECORD=your_record
+ZONEID=your_zoneid
+RECORDID=your_recordid
+
+## Get the current public IP address
+IP=$(curl -s https://cloudflare.com/cdn-cgi/trace | grep -E '^ip' | cut -d = -f 2)
+
+## Get the current IP address on Cloudflare
+CF_IP=$(curl -s https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$RECORDID \
+  -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+  -H "Authorization: Bearer $CLOUDFLARE_API_KEY" \
+  -H "Content-Type: application/json" \
+  | jq '.result.content' \
+  | tr -d \")
+
+## Update the IP address on Cloudflare if it has changed
+if [ "$IP" != "$CF_IP" ]; then
+  curl -s https://api.cloudflare.com/client/v4/zones/$ZONEID/dns_records/$RECORDID \
+    -X PATCH \
+    -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+    -H "Authorization: Bearer $CLOUDFLARE_API_KEY" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"A\",\"name\":\"$RECORD\",\"content\":\"$IP\"}"
+fi
